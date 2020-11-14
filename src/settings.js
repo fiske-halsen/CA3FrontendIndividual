5f74b3384bf060ac6c029e6cd974c4735a1cab49
Phillip Andersen <phillip_andersen@hotmail.com>
2020-11-14 23:54:47 +0100

    Deployed backend url added

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
-let URL = "http://localhost:8080/jpareststarter";
-
+//let URL = "http://localhost:8080/jpareststarter";
+let URL = "https://hamderelskercode.com/tomcat/ca3_backendss";
 export { URL };
